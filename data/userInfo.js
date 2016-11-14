ecd68bee08123d8b9c759959c7a4755d19f5e7c4
mengzhihenji <1098289124@qq.com>
2016-11-15 01:00:59 +0800

    first commit

---
+++ data/userInfo.js
@@ -0,0 +1,35 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "赵梦梦" , 
+    userPortrait:"images/icon/intro_03.png",
+    jobWant:  "web",
+    userQQ:  "1098289124",
+    userEmail:   "1098289124@qq.com",
+    motto:  "道可道，非常道；名可名，非常名", 
+    userPhone: 18310055985,
+    userOrignPlace:  "河北省石家庄市藁城区" ,
+    userSeatPlace:  "北京市",
+    userWeibo: "第7朵盛开d花上",
+    
+}
+
